b76b62147586b8bd9d115a5bc01ad904e55a90a9
Кирилл Никитин <gr414_nkyu@lab-404-10.tomtit-tomsk.ru>
2024-04-15 11:43:43 +0700

    Реализовано 2 тестов

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://dev.profteam.su/')
-  })
-  it('Должна успешно загрузиться главная страница', () => {
-    cy.visit('https://dev.profteam.su/');
-    cy.url().should('include', '/');
-  });
-  it('Следует найти и подтвердить наличие определенного элемента', () => {
-    cy.visit('https://dev.profteam.su/');
-    cy.get('.page-title').should('be.visible');
-  });
+describe('template spec', () => { 
+ 
   it('Переход на страницу по ссылке', () => {
     cy.visit('https://dev.profteam.su/');
     cy.get(':nth-child(1) > .header__nav > [href="/vacancies"] > .header__label').click();
@@ -21,7 +11,7 @@ describe('template spec', () => {
     cy.get('.form-input--password').type('Password1');
     cy.wait(2000); 
     cy.get('.form__buttons > :nth-child(3)').click();
-    cy.get('.menu-item__name').should('be.visible');
+    cy.url().should('eq', 'https://dev.profteam.su/account/main');
   });
   
 })
