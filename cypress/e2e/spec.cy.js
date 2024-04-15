5f44f12c88c431a7f883d62c9a09ba114b45f03f
RederikLimp <kirill.alisa.2004@mail.ru>
2024-04-15 21:05:31 +0700

    Готово6

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -38,25 +38,40 @@ describe('template spec', () => {
 //     cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type('Ахмедоато');
 //     cy.get(':nth-child(3) > .button').click();
 //   });
-  it('Проверка создания потребности', () => {
-    cy.visit('https://dev.profteam.su/login#b');
-    cy.get('.form-input--text').type('testerEmployer');
-    cy.get('.form-input--password').type('Password1');
-    cy.wait(1000); 
+//   it('Проверка создания потребности', () => {
+//     cy.visit('https://dev.profteam.su/login#b');
+//     cy.get('.form-input--text').type('testerEmployer');
+//     cy.get('.form-input--password').type('Password1');
+//     cy.wait(1000); 
 
-    cy.get('.form__buttons > :nth-child(3)').click();
-    cy.wait(2000); 
+//     cy.get('.form__buttons > :nth-child(3)').click();
+//     cy.wait(2000); 
 
-    cy.visit('https://dev.profteam.su/account/needs#b');
-    cy.get('.needs-block__filters-wrapper > .button').click();
+//     cy.visit('https://dev.profteam.su/account/needs#b');
+//     cy.get('.needs-block__filters-wrapper > .button').click();
 
-    cy.get('.desktop-modal__content > .vacancy-need-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(1) > .form-control--responsive > .form-input--text').type('Наливайщик чая.');
-    cy.get('.desktop-modal__content > .vacancy-need-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(3) > .form-control > .form-area').type('Что б чай наливал');
-    cy.get('.desktop-modal__content > .vacancy-need-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(4) > .form-control > .form-area').type('Умение пользоваться чайником, и выкидывать заварку.  АААА ещё чтоб забавлял холодной водичкой.');
+//     cy.get('.desktop-modal__content > .vacancy-need-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(1) > .form-control--responsive > .form-input--text').type('Наливайщик чая.');
+//     cy.get('.desktop-modal__content > .vacancy-need-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(3) > .form-control > .form-area').type('Что б чай наливал');
+//     cy.get('.desktop-modal__content > .vacancy-need-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(4) > .form-control > .form-area').type('Умение пользоваться чайником, и выкидывать заварку.  АААА ещё чтоб забавлял холодной водичкой.');
     
-    cy.wait(2000); 
-    cy.get('.desktop-modal__content > .vacancy-need-wrapper > .form > .form__buttons > .button').click();
-});
+//     cy.wait(2000); 
+//     cy.get('.desktop-modal__content > .vacancy-need-wrapper > .form > .form__buttons > .button').click();
+// });
+// it('Проверка процесса регистрации', () => {
+//       cy.visit('https://dev.profteam.su/registration');
+  
+//       cy.get('.button-login__network-city-desktop').click();
+
+//       cy.get('.form__labels > :nth-child(1) > .form-control--medium > .form-input--text').type('newUser');
+//       cy.get(':nth-child(2) > .form-control--medium > .form-input--password').type('Password12А3');
+  
+//       cy.get('.login-form__button > .button').click();
+//   //Проверка зарегестрирован ли пользователь
+
+//   cy.get('.form-input--text').type('newUser');
+//   cy.get('.form-input--password').type('Password12А3');
+//   cy.get('.form__buttons > :nth-child(3)').click(1000);
+//     });
 })
 
 
